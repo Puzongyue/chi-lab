a178a48e57240d456737f230e7d7cf3d002021cd
wxj <kingsnowmushroom@126.com>
2020-12-14 16:10:27 +0800

    add：个人中心布局

---
+++ frontend/cinema/src/components/UserCenter/BasicInfo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        BasciInfo
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BasicInfo",
+    data(){
+        return {}
+    }
+}
+</script>
---
+++ frontend/cinema/src/components/UserCenter/UserTicket.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="user-ticket">
+        Ticket List
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserTicket",
+    data(){
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+.user-ticket{
+    
+}
+</style>
