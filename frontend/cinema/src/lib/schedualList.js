31601a1c0ab79f621804b79a4fcba6fa5c76cfac
wxj <kingsnowmushroom@126.com>
2020-12-16 17:19:09 +0800

    update：排片添加电影id

--- frontend/cinema/src/lib/schedualList.js
+++ frontend/cinema/src/lib/schedualList.js
@@ -3,7 +3,7 @@ const curTime = new Date();
 const schedualList = [
   {
     id: "0",
-    movieId: "0",
+    movieId: 0,
     hallId: 0,
     startTime: curTime.setTime(curTime.setHours(curTime.getHours() + 1)),
     endTime: curTime.setTime(curTime.setHours(curTime.getHours() + 4)),
@@ -12,42 +12,47 @@ const schedualList = [
   },
   {
     id: "1",
+    movieId: 0,
+    hallId: 2,
     startTime: curTime.setTime(curTime.setHours(curTime.getHours() + 3)),
     endTime: curTime.setTime(curTime.setHours(curTime.getHours() + 5)),
     language: "原版3D",
-    hallId: 2,
     prize: 34.0
   },
   {
     id: "2",
+    movieId: 0,
+    hallId: 2,
     startTime: curTime.setTime(curTime.setHours(curTime.getHours() + 10)),
     endTime: curTime.setTime(curTime.setHours(curTime.getHours() + 14)),
     language: "原版3D",
-    hallId: 2,
     prize: 35.0
   },
   {
     id: "3",
+    movieId: 0,
+    hallId: 2,
     startTime: curTime.setTime(curTime.setHours(curTime.getHours() + 24)),
     endTime: curTime.setTime(curTime.setHours(curTime.getHours() + 25)),
     language: "原版3D",
-    hallId: 2,
     prize: 36.0
   },
   {
     id: "4",
+    movieId: 0,
+    hallId: 1,
     startTime: curTime.setTime(curTime.setHours(curTime.getHours() + 45)),
     endTime: curTime.setTime(curTime.setHours(curTime.getHours() + 47)),
     language: "原版3D",
-    hallId: 1,
     prize: 37.0
   },
   {
     id: "5",
+    movieId: 0,
+    hallId: 0,
     startTime: curTime.setTime(curTime.setHours(curTime.getHours() + 48)),
     endTime: curTime.setTime(curTime.setHours(curTime.getHours() + 50)),
     language: "原版3D",
-    hallId: 0,
     prize: 38.0
   }
 ];
